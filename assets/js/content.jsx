ccc99ee09a048fa94fc6466d576e5b98a8672ce9
clanofnoobs <samiulg3@gmail.com>
2016-11-26 18:42:51 -0800

    get YouTube player API

--- assets/js/content.jsx
+++ assets/js/content.jsx
@@ -2,3 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 console.log("Hello world!");
+
+var init = function() {
+  var youtube_player_el = $("#player-api");
+  var HTTPlinkToCaptions = youtube_player_el.childNodes[0].captionTracks[0].G
+}
+
+init();
