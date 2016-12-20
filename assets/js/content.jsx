81917b2e65f35f577dda358f3edc292c69a0aaca
clanofnoobs <samiulg3@gmail.com>
2016-12-19 20:08:27 -0800

    Find YouTube player API

--- assets/js/content.jsx
+++ assets/js/content.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log("Hello world!");
-
 var init = function() {
-  var youtube_player_el = $("#player-api");
-  var HTTPlinkToCaptions = youtube_player_el.childNodes[0].captionTracks[0].G
-}
+  var HTTPlinkToCaptions = {};
 
-init();
+  var youtube_player_el = $("#player-api");
+  if (youtube_player_el.childNodes) {
+  } else {
+    return;
+  }
+
+  var audioTrack = youtube_player_el.childNodes[0].getAudioTrack();
+
+  if (audioTrack.captionTracks.length > 0) {
+    HTTPlinkToCaptions = audioTrack.captionTracks;
+  }
+
+  console.log(HTTPlinkToCaptions);
+};
